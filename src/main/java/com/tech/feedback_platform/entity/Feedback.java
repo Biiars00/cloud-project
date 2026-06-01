e07e1743d9b1cb3d48cbade3d4d49de78d451bba
Beatriz Ribeiro <biiaribeiro60@gmail.com>
2026-06-01 13:22:36 -0300

    refactor: entity

--- src/main/java/com/tech/feedback_platform/entity/Feedback.java
+++ src/main/java/com/tech/feedback_platform/entity/Feedback.java
@@ -1,34 +1,18 @@
 package com.tech.feedback_platform.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDateTime;
-
-@Entity
-@Table(name = "feedbacks")
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class Feedback {
-
-    @Id
     private String id;
-
     private String descricao;
-
     private Integer nota;
-
-    @Enumerated(EnumType.STRING)
-    private UrgencyLevel urgencia;
-
-    private LocalDateTime dataEnvio;
+    private String urgencia; // OK, ATENÇÃO, CRÍTICA
+    private String dataEnvio;
 }
