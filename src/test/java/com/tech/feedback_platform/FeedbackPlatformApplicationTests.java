88b081c18ae466dabd766d77a32f6c195342c885
Beatriz Ribeiro <biiaribeiro60@gmail.com>
2026-05-31 23:06:12 -0300

    feat: FeedbackPlatformApplication.test

---
+++ src/test/java/com/tech/feedback_platform/FeedbackPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tech.feedback_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeedbackPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
